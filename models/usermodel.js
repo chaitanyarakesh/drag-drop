70828e8e9a585ab7f12dc351c775c2ceedb3533e
chaitanyarakesh <vinaymca2015@gmail.com>
2018-03-29 21:14:25 +0530

    update

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,11 +1,43 @@
 module.exports.saveUser = function(userobj,db,callback){
-    db.collection('users').insertOne(userobj,function(err,result){
+	console.log(userobj)
+/*     db.collection('users').insertOne( {"properties": userobj},{  safe: true, upsert: true},function(err,result){
         if(err){
+			console.log(err)
          return callback(err);
         }else{
+			console.log(result)
         return callback(result);
         }
-    })
+    }) */
+	console.log(userobj.chkId)
+	console.log({ "modal_properties.chkId": userobj.chkId })
+	db.collection('user').findOne({ "modal_properties.chkId": userobj.chkId },function(err,result){
+		console.log(result)
+		if(err){
+			console.log(err)
+		}
+		else if(result){
+			db.collection('users').update({ "modal_properties.chkId": userobj.chkId },{$push:{"modal_properties": userobj}},{  safe: true, upsert: true},function(err,result){
+				if(err){
+					console.log(err)
+				 return callback(err);
+				}else{
+				return callback(result);
+				}
+			})
+		}
+		else{
+			db.collection('users').insertOne( {"modal_properties": {userobj}},function(err,result){
+				if(err){
+					console.log(err)
+				 return callback(err);
+				}else{
+					console.log("inserted")
+				return callback(result);
+				}
+			})
+		}
+	})
 }
  module.exports.getUserInfo=function(db,callback){
     //  db.collection('users').findOne({and:[{email:userobj.email},{password:userobj.password}]},function(err,result){
